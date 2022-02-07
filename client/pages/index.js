789768e22f6ddcb7301f94efe785238bd84bb94d
kanika52 <57388028+kanika52@users.noreply.github.com>
2022-02-08 00:22:44 +0530

    Update index.js

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,11 +1,10 @@
-const Index = () => {
-    return ( <
-        >
-        <
-        h1 > Hello World < /h1> <
-        p > this is client side < /p> <
-        />
-    );
+const Index = ()=>{
+  return (
+    <>
+    <h1> Hello World</h1>
+    <p>this is client side </p>
+    </>
+  );
 };
 
 export default Index;
