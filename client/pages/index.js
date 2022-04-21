93a4fe483bf16051a360e8053b2404148a17ddd7
kanika52 <57388028+kanika52@users.noreply.github.com>
2022-04-22 03:10:09 +0530

    Homepage design

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,11 +1,34 @@
+import React, {useEffect, useRef } from 'react';
+import lottie from 'lottie-web';
 
 const Index = () => {
+	
+	const container = useRef(null)
+	
+	useEffect(() => {
+		lottie.loadAnimation({
+			container: container.current,
+			renderer:'svg',
+			loop:true,
+			autoplay: true,
+			animationData: require('./office.json')
+			
+			
+		})
+	}, [])
     return (
-        <><header className = "jumbotron text-center mt-1 pt-2 pb-5 my-auto bg-primary square">
-            <h1>Integrated Learning System</h1> 
-             </header>
-             <p> this is client side </p> 
-        
+        <>
+		     <div className= "app">
+			   <div id="heading"><h1>Welcome to WeGrow</h1>
+			     <div id="heading2">
+				 <h6>Think out of the box and create a learning experience</h6>
+				
+                  <div id= 'lottieContainer'>
+				      <div className="container " ref = {container}></div>
+			     </div>
+				</div>
+			   </div>
+			 </div>
         </>
     );
 };
